fd140a81ee1769b90fb11eee6a47ccc4cf46d979
Armando Torres <armando.tinez@icloud.com>
2023-06-02 17:28:00 +0200

    Refactor exception handler into shared domain to handle api business layer exceptions

--- src/main/java/com/tasksrest/api/shared/infrastructure/rest/RestExceptionHandler.java
+++ src/main/java/com/tasksrest/api/shared/infrastructure/rest/RestExceptionHandler.java
@@ -1,15 +1,15 @@
 package com.tasksrest.api.shared.infrastructure.rest;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import com.tasksrest.api.shared.application.ErrorResponse;
-import com.tasksrest.api.shared.domain.Error;
+import com.tasksrest.api.shared.domain.vo.RestError;
+import com.tasksrest.api.task.domain.exception.DuplicateTaskException;
+import com.tasksrest.api.task.domain.exception.InvalidStatusException;
+import com.tasksrest.api.task.domain.exception.NotFoundTaskException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,14 +17,34 @@ import org.slf4j.LoggerFactory;
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler{
 
-    private final Logger logger = LoggerFactory.getLogger(ExceptionHandler.class);
-    
-    @ExceptionHandler
-    protected ResponseEntity<Object> handleConflict(Exception e, WebRequest request) {
-        Error errorResponse = (new ErrorResponse()).invoke(e.getMessage());
+    private final Logger logger = LoggerFactory.getLogger(RestExceptionHandler.class);
 
-        this.logger.warn(String.format("%s", errorResponse.getCause()));
+    @ExceptionHandler({DuplicateTaskException.class})
+    protected ResponseEntity<Object> handleDuplicateTaskException(DuplicateTaskException e) {
+        this.logException(HttpStatus.CONFLICT.value(), e.getMessage());
         
-        return handleExceptionInternal(e, errorResponse, new HttpHeaders(), HttpStatus.UNPROCESSABLE_ENTITY, request);
+        return this.resposeException(HttpStatus.CONFLICT, e.getMessage());
+    }
+
+    @ExceptionHandler({NotFoundTaskException.class})
+    protected ResponseEntity<Object> handleNotFoundTaskException(NotFoundTaskException e) {
+        this.logException(HttpStatus.NOT_FOUND.value(), e.getMessage());
+        
+        return this.resposeException(HttpStatus.NOT_FOUND, e.getMessage());
+    }
+
+    @ExceptionHandler({InvalidStatusException.class})
+    protected ResponseEntity<Object> handleInvalidStatusException(InvalidStatusException e) {
+        this.logException(HttpStatus.UNPROCESSABLE_ENTITY.value(), e.getMessage());
+
+        return this.resposeException(HttpStatus.UNPROCESSABLE_ENTITY, e.getMessage());
+    }
+
+    private void logException(Integer code, String cause) {
+        this.logger.warn(String.format("%d %s", code, cause));
+    }
+
+    private ResponseEntity<Object> resposeException(HttpStatus status, String cause) {
+        return ResponseEntity.status(status).body(new RestError(cause));
     }
 }
