c2433b8c96bb5e4b517082e8a36ff2a5cc71b8a8
Armando Torres <armando.tinez@icloud.com>
2023-06-02 16:49:35 +0200

    Refactor create task to get request params as json mapped into request object

---
+++ src/main/java/com/tasksrest/api/task/application/CreateTaskRequest.java
@@ -0,0 +1,33 @@
+package com.tasksrest.api.task.application;
+
+public class CreateTaskRequest {
+    private String name;
+
+    private String desc;
+
+    private String status;
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String value){
+        this.name = value;
+    }
+
+    public String getDesc(){
+        return this.desc;
+    }
+
+    public void setDesc(String value){
+        this.desc = value;
+    }
+
+    public String getStatus(){
+        return this.status;
+    }
+
+    public void setStatus(String value){
+        this.status = value;
+    }
+}
