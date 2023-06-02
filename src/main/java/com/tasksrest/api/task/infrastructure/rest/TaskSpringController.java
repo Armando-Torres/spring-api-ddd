b9c73103cf41864c9a0ffdcfc6d7dc3f5dfdf0ec
Armando Torres <armando.tinez@icloud.com>
2023-06-02 16:49:35 +0200

    Refactor create task to get request params as json mapped into request object

--- src/main/java/com/tasksrest/api/task/infrastructure/rest/TaskSpringController.java
+++ src/main/java/com/tasksrest/api/task/infrastructure/rest/TaskSpringController.java
@@ -1,27 +1,34 @@
 package com.tasksrest.api.task.infrastructure.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 import com.tasksrest.api.task.application.CreateTask;
+import com.tasksrest.api.task.application.CreateTaskRequest;
+import com.tasksrest.api.task.application.GetTask;
 import com.tasksrest.api.task.domain.Task;
 import com.tasksrest.api.task.domain.TaskRepository;
 
 @RestController
-@RequestMapping(value = "/v1/task")
+@RequestMapping(value = "/v1/task", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 public class TaskSpringController {    
     @Autowired
     private TaskRepository repository;
 
     @PostMapping
-    public Task createSingleTask(@RequestParam("name") String name, @RequestParam("desc") String description, @RequestParam("status") String status){
+    //public Task createSingleTask(@RequestBody String name, @RequestBody String description, @RequestBody String status){
+    public Task createSingleTask(@RequestBody CreateTaskRequest requestBody){
         CreateTask useCase = new CreateTask(this.repository);
         
-        Task task = useCase.invoke(name, description, status);
+        Task task = useCase.invoke(requestBody);
 
         return task;
     }
