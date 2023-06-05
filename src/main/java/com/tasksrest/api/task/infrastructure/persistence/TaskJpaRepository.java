e3c440a82a9700e4c43f24fb4a43793c3ccd3166
Armando Torres <armando.tinez@icloud.com>
2023-06-05 09:51:06 +0200

    Refactor Task jpa interface repository to composition

--- src/main/java/com/tasksrest/api/task/infrastructure/persistence/TaskJpaRepository.java
+++ src/main/java/com/tasksrest/api/task/infrastructure/persistence/TaskJpaRepository.java
@@ -1,12 +1,9 @@
 package com.tasksrest.api.task.infrastructure.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.tasksrest.api.task.domain.Task;
-import com.tasksrest.api.task.domain.TaskRepository;
 
-@Repository
-public interface TaskJpaRepository extends JpaRepository<Task, Integer>, TaskRepository {
+public interface TaskJpaRepository extends JpaRepository<Task, Integer> {
     
 }
