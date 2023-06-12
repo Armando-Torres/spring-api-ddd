be3f08c116d5cd6663220b2e89bbcf2c3e22a3b4
Armando Torres <armando.tinez@icloud.com>
2023-06-12 17:17:29 +0200

    Add task container abstraction into shared

--- src/main/java/com/tasksrest/api/kanban/domain/KanbanColumn.java
+++ src/main/java/com/tasksrest/api/kanban/domain/KanbanColumn.java
@@ -3,49 +3,15 @@ package com.tasksrest.api.kanban.domain;
 import com.tasksrest.api.shared.domain.TaskContainer;
 
 public class KanbanColumn extends TaskContainer{
-    private Integer id;
-
-    private Integer order;
-
-    private String name;
-
     private Integer wip;
 
     private Kanban kanban;
 
-    public KanbanColumn() {
+    public KanbanColumn() {}
 
-    }
-
-    public KanbanColumn(Integer order, String name, Integer wip, Kanban kanban) {
-        this.order = order;
-        this.name = name;
+    public KanbanColumn(Integer wip, Kanban kanban) {
         this.wip = wip;
         this.kanban = kanban;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getId() {
-        return this.id;
-    }
-
-    public void setOrder(Integer order) {
-        this.order = order;
-    }
-
-    public Integer getOrder() {
-        return this.order;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return this.name;
     }
 
     public void setWip(Integer wip) {
