521fabcc94cc250a4f11553a1f5230f3a9d03c4a
Armando Torres <armando.tinez@icloud.com>
2023-06-15 11:53:25 +0200

    Refactor to allow create kanban with columns if are present on request data

--- src/main/java/com/tasksrest/api/kanban/infrastructure/rest/KanbanSpringController.java
+++ src/main/java/com/tasksrest/api/kanban/infrastructure/rest/KanbanSpringController.java
@@ -1,5 +1,7 @@
 package com.tasksrest.api.kanban.infrastructure.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,10 +13,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tasksrest.api.kanban.application.AddColumn;
 import com.tasksrest.api.kanban.application.CreateKanban;
 import com.tasksrest.api.kanban.application.GetKanban;
+import com.tasksrest.api.kanban.application.service.ColumnResponse;
 import com.tasksrest.api.kanban.application.service.CreateKanbanRequest;
 import com.tasksrest.api.kanban.application.service.KanbanResponse;
+import com.tasksrest.api.kanban.domain.ColumnRepository;
 import com.tasksrest.api.kanban.domain.KanbanRepository;
 
 @RestController
@@ -23,11 +28,22 @@ public class KanbanSpringController {
     @Autowired
     private KanbanRepository kanbanRepository;
 
+    @Autowired
+    private ColumnRepository columnRepository;
+
     @PostMapping
     public ResponseEntity<KanbanResponse> createKanban(@RequestBody CreateKanbanRequest requestBody){
         CreateKanban useCase = new CreateKanban(this.kanbanRepository);
         
         KanbanResponse kanban = useCase.invoke(requestBody);
+
+        if (requestBody.getColumns().size() > 0) {
+            AddColumn addColumnUseCase = new AddColumn(this.columnRepository, this.kanbanRepository);
+        
+            List<ColumnResponse> kanbanColumns = addColumnUseCase.invoke(kanban.getId(), requestBody.getColumns());
+
+            kanban.setColumns(kanbanColumns);
+        }
 
         return ResponseEntity.status(HttpStatus.CREATED).body(kanban);
     }
