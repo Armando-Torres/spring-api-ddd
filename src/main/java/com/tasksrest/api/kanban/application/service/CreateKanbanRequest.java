5bf97a8a7dcd50408ced3332723952df638ca0bc
Armando Torres <armando.tinez@icloud.com>
2023-06-15 20:02:14 +0200

    Refactor kanban application services to allow return response with columns or without it if at create endpoint are specified

--- src/main/java/com/tasksrest/api/kanban/application/service/CreateKanbanRequest.java
+++ src/main/java/com/tasksrest/api/kanban/application/service/CreateKanbanRequest.java
@@ -3,18 +3,8 @@ package com.tasksrest.api.kanban.application.service;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CreateKanbanRequest {
-    private String name;
-
+public class CreateKanbanRequest extends KanbanRequest {
     private List<ColumnRequest> columns = new ArrayList<ColumnRequest>();
-
-    public String getName(){
-        return this.name;
-    }
-
-    public void setName(String value){
-        this.name = value;
-    }
 
     public List<ColumnRequest> getColumns() {
         return this.columns;
--- src/main/java/com/tasksrest/api/kanban/application/service/KanbanResponse.java
+++ src/main/java/com/tasksrest/api/kanban/application/service/KanbanResponse.java
@@ -1,22 +1,15 @@
 package com.tasksrest.api.kanban.application.service;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
-import com.tasksrest.api.kanban.domain.Column;
 import com.tasksrest.api.kanban.domain.Kanban;
 
 public class KanbanResponse {
     private Integer id;
     
     private String name;
-
-    private Collection<ColumnResponse> columns;
     
     public KanbanResponse(Kanban kanban) {
         this.id = kanban.getId();
         this.name = kanban.getName();
-        this.columns = this.columnsToColumnResponse(kanban.getColumns());
     }
 
     public Integer getId() {
@@ -25,25 +18,5 @@ public class KanbanResponse {
 
     public String getName() {
         return this.name;
-    }
-
-    public void setColumns(Collection<ColumnResponse> kanbanColumns) {
-        this.columns = kanbanColumns;
-    }
-
-    public Collection<ColumnResponse> getColumns() {
-        return this.columns;
-    }
-
-    private Collection<ColumnResponse> columnsToColumnResponse(Collection<Column> columns) {
-        Collection<ColumnResponse> kanbanColumns = new ArrayList<ColumnResponse>();
-
-        for (Column column : columns) {
-            ColumnResponse columnResponse = new ColumnResponse(column);
-
-            kanbanColumns.add(columnResponse);
-        }
-
-        return kanbanColumns;
     }
 }
