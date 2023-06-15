08a825dd6536b16bc6b2ef0347cf703430e65fd4
Armando Torres <armando.tinez@icloud.com>
2023-06-15 18:18:25 +0200

    Add get columns for kanban id endpoint

--- src/main/java/com/tasksrest/api/kanban/infrastructure/rest/ColumnSpringController.java
+++ src/main/java/com/tasksrest/api/kanban/infrastructure/rest/ColumnSpringController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tasksrest.api.kanban.application.AddColumn;
+import com.tasksrest.api.kanban.application.GetColumns;
 import com.tasksrest.api.kanban.application.service.AddColumnRequest;
 import com.tasksrest.api.kanban.application.service.ColumnResponse;
 import com.tasksrest.api.kanban.domain.ColumnRepository;
@@ -34,6 +36,15 @@ public class ColumnSpringController {
         List<ColumnResponse> kanbanColumn = useCase.invoke(kanbanId, requestBody);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(kanbanColumn);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<ColumnResponse>> getKanbanColumns(@PathVariable("id") Integer kanbanId) {
+        GetColumns useCase = new GetColumns(this.columnRepository, this.kanbanRepository);
+        
+        List<ColumnResponse> kanbanColumn = useCase.invoke(kanbanId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(kanbanColumn);
     }
 }
 
